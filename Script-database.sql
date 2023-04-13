61e65c7ed6474d8dea97ab52732c1ed9609597ff
Santiago Forero <santiago.forero-y@mail.escuelaing.edu.co>
2023-04-12 23:19:41 -0500

    avance conexión base de datos

---
+++ Script-database.sql
@@ -0,0 +1,7 @@
+-- cvds_8.Configuration definition
+
+CREATE TABLE `Configuration` (
+  `configuration_id` int DEFAULT NULL,
+  `propiedad` varchar(100) DEFAULT NULL,
+  `valor` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
